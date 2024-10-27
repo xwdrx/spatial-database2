2b0d1944edce95fa66db52bfdccacb3ddec58177
xwdrx <79997873+xwdrx@users.noreply.github.com>
2024-10-27 14:53:37 +0100

    lab3 up

---
+++ SQLQuery1_402789.sql
@@ -0,0 +1,24 @@
+USE AdventureWorksDW2022;
+GO
+
+IF OBJECT_ID('GetCurrencyRatesByYearsAgo', 'P') IS NOT NULL
+    DROP PROCEDURE GetCurrencyRatesByYearsAgo;
+GO
+
+CREATE PROCEDURE GetCurrencyRatesByYearsAgo
+    @YearsAgo INT
+AS BEGIN
+    SELECT 
+        fcr.*,
+        dc.CurrencyAlternateKey
+    FROM 
+        FactCurrencyRate AS fcr
+    INNER JOIN 
+        DimCurrency AS dc ON fcr.CurrencyKey = dc.CurrencyKey
+    WHERE 
+    CONVERT(DATE, CONVERT(VARCHAR, fcr.DateKey)) <= DATEADD(YEAR, -@YearsAgo, GETDATE())
+    AND dc.CurrencyAlternateKey IN ('GBP', 'EUR')
+END;
+
+EXEC GetCurrencyRatesByYearsAgo @YearsAgo = 8;
+
