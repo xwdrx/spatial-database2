968f7477e85e96bf3b8114bcb3d502dafa43b850
xwdrx <79997873+xwdrx@users.noreply.github.com>
2024-11-02 15:20:49 +0100

    lab4 up

---
+++ queries_task2.sql
@@ -0,0 +1,6 @@
+--Query written for Oracle database
+SELECT * FROM ALL_TAB_COLUMNS WHERE TABLE_NAME = 'FactInternetSales';
+--Query written for PostgreSQL database
+SELECT * FROM information_schema.columns WHERE table_name = 'FactInternetSales';
+--Query written for MySQL database
+SELECT * FROM information_schema.columns WHERE table_name = 'FactInternetSales';
